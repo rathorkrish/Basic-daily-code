f9c2c002a11613cd72ce534b3c3a88ae69f83f08
Krishna rathor <rathorkrishna142@gmail.com>
2026-06-13 19:53:16 +0530

    AbstractionDemo.

---
+++ AbstractionDemo.java
@@ -0,0 +1,26 @@
+abstract class Animal {
+
+    abstract void sound();
+
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractionDemo {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+        d.sleep();
+    }
+}
